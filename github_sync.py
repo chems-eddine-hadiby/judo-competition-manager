49c786b4a65c154e320db010a3dbf238b6dcd885
chems-eddine-hadiby <chemsseddine.hdiby@gmail.com>
2026-03-15 14:07:07 +0100

    update sync mechanism

--- github_sync.py
+++ github_sync.py
@@ -12,7 +12,7 @@ import urllib.error
 REPO = "chems-eddine-hadiby/judo-competitions"
 API_ROOT = f"https://api.github.com/repos/{REPO}/contents"
 
-def _request(method, path, token="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", data=None):
+def _request(method, path, token, data=None):
     url = f"{API_ROOT}/{path}".rstrip("/")
     headers = {
         "Accept": "application/vnd.github+json",
